30ec1859b240c144369b2ec3a825e72636956f55
Coder-dev-sys <dvvyas652@gmail.com>
2025-03-08 20:23:17 +0530

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import Alert from './Components/alert';
-// import About from './Components/About';
+import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import React, { useState } from 'react';
+import  {
+  BrowserRouter as Router ,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
-  const [mode,setMode] = useState('light');
-  const [alert,setAlert] = useState(null);
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type) => {
-    setAlert  ({
+  const showAlert = (message, type) => {
+    setAlert({
       msg: message,
       type: type
     })
@@ -19,44 +24,50 @@ function App() {
     }, 1500);
   }
   const toggleMode = () => {
-    if(mode === 'light' )
-    {
-        setMode('dark');
-        document.body.style.backgroundColor = '#143D60';
-        document.body.style.color = 'white';
-        showAlert("Dark Mode Enabled","success");
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#143D60';
+      document.body.style.color = 'white';
+      showAlert("Dark Mode Enabled", "success");
     }
-    else{
-        setMode('light');
-        document.body.style.backgroundColor = 'white';
-        document.body.style.color = 'black';
-        showAlert("Light Mode Enabled","success");
-    } 
-  }  
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+      showAlert("Light Mode Enabled", "success");
+    }
+  }
 
   const maroonMode = () => {
     setMode('dark');
     document.body.style.backgroundColor = '#780C28';
     document.body.style.color = 'white';
-    showAlert("Maroon Mode Enabled","success");
+    showAlert("Maroon Mode Enabled", "success");
   }
 
   const purpleMode = () => {
     setMode('dark');
     document.body.style.backgroundColor = '#441752';
     document.body.style.color = 'white';
-    showAlert("Purple Mode Enabled","success");
+    showAlert("Purple Mode Enabled", "success");
   }
   return (
-      <>
-        <Navbar title="TextUtils" about="About TextUtils" mode={mode} maroonMode={maroonMode} purpleMode={purpleMode} toggleMode={toggleMode}/>
-        <Alert alert={alert}/>
+    <>
+        <Router>
+          
+        <Navbar title="TextUtils" about="About" mode={mode} maroonMode={maroonMode} purpleMode={purpleMode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
         <div className="container my-3">
-          {/* <About/> */}
-          <TextForm showAlert={showAlert} heading="Enter Text To Analyze : " mode={mode}/>
+          <Routes>
+            
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter Text To Analyze : " mode={mode} />}></Route>
+            
+          </Routes> 
         </div>
-      </>
-    );
+        </Router>
+    </>
+  );
 }
 
 export default App;
